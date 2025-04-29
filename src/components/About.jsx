794dbf39fbed52effb6fc2e3af8aa88e6f0712bc
anurag.lohar <anurag.lohar@celebaltech.com>
2025-04-29 15:39:40 +0530

    minor ui update

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,13 @@
 import React from "react";
 
 export default function About() {
-    return <div className="text-center text-2xl mt-20">📘 About This Project</div>;
+  return (
+    <div className="text-center flex flex-col items-center justify-center gap-y-5 h-[80vh] bg-gray-100 dark:bg-gray-900">
+      <h2 className="text-4xl font-bold text-gray-800 dark:text-white ">
+        DevOps trainee - AK
+      </h2>
+      <p>Docker Hands on practice</p>
+    </div>
+  )
   }
   
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export default function Home() {
-  return <div className="text-center text-2xl mt-20">🏠 Welcome Home</div>;
+  return (
+    <div className="text-center flex flex-col items-center justify-center h-[80vh] bg-gray-100 dark:bg-gray-900">
+      <h2 className="text-4xl font-bold text-gray-800 dark:text-white">
+      🏠 Welcome Home
+      </h2>
+    </div>
+  )
 }
