ee87b101c10b5415c0ca8bd8f3f813548c076d42
anurag.lohar <anurag.lohar@celebaltech.com>
2025-06-12 14:24:05 +0530

    minor updates in Dev branch

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="text-center flex flex-col items-center justify-center h-[80vh] bg-gray-100 dark:bg-gray-900">
       <h2 className="text-4xl font-bold text-gray-800 dark:text-white">
-      🏠 Welcome Home
+      🏠 Welcome Home - Dev Branch
       </h2>
     </div>
   )
